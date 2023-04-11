6a9a9e6e062e5656ed7941be4b4cc3aa52e145b0
Andrii_Barsuk <Andrii_Barsuk@epam.com>
2023-04-11 16:54:14 +0300

    fourth test commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Second commit");
         System.out.println("Third commit");
+        System.out.println("fourth commit");
     }
 }
